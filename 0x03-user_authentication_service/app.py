51dade71dca809a5189e363ccd896fcb906ef8db
Y-admen <yassminash305@gmail.com>
2024-09-12 18:39:07 +0300

    Credentials validation

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app with user registration
+"""
+from flask import Flask, request, jsonify
+from auth import Auth
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route("/", methods=["GET"])
+def welcome():
+    """
+    GET route that returns a JSON payload
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def users():
+    """
+    POST /users route to register a new user
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -51,3 +51,20 @@ class Auth:
             hashed_password = _hash_password(password)
             user = self._db.add_user(email, hashed_password)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Validate login credentials
+
+        Args:
+            email (str): The email of the user
+            password (str): The password of the user
+
+        Returns:
+            bool: True if the credentials are valid, False otherwise
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(), user.hashed_password)
+        except NoResultFound:
+            return False

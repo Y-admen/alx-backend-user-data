ca507fe0339ed710f202a9bcc1998dd678ba004e
Y-admen <yassminash305@gmail.com>
2024-09-12 18:29:54 +0300

    Hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Auth module
+"""
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """
+    Hash a password using bcrypt
+
+    Args:
+        password (str): The password to hash
+
+    Returns:
+        bytes: The salted hash of the password
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+    return hashed_password

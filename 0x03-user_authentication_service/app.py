77294ad6c51c14a76963d4a50c5a6e165d40360b
Y-admen <yassminash305@gmail.com>
2024-09-12 18:54:11 +0300

    Find user by session ID

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -55,15 +55,15 @@ def logout():
     DELETE /sessions route to log out a user
     """
     session_id = request.cookies.get("session_id")
-    
+
     if session_id is None:
         abort(403)
-    
+
     user = AUTH.get_user_from_session_id(session_id)
-    
+
     if user is None:
         abort(403)
-    
+
     AUTH.destroy_session(user.id)
     return redirect("/")
 
